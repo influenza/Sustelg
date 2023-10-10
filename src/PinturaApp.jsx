2d59982cd03c3bbb8cd12c38230766744c13452c
influenza <118693099+influenza@users.noreply.github.com>
2023-10-10 08:26:26 -0300

    Update PinturaApp.jsx

--- src/PinturaApp.jsx
+++ src/PinturaApp.jsx
@@ -5,10 +5,10 @@ import { useState, useEffect } from "react";
 
 export default function PinturaApp(){
     const [larguraTabela, SetTabela] = useState(window.innerWidth)
-    const [abelha, SetAbelha] = useState([])
-    const [seila, Setseila] = useState([])
-    const [quadro, Setquadro] = useState([])
-
+    const [abelha, SetAbelha] = useState([80,80])
+    const [seila, Setseila] = useState([150,150])
+    const [quadro, Setquadro] = useState([125,125])
+    
   useEffect(()=>{
     console.log(larguraTabela)
     const svg =()=>{
