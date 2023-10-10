cd041e36f24ce283f30992790db679e09f61c981
influenza <118693099+influenza@users.noreply.github.com>
2023-10-10 19:27:04 -0300

    Update PinturaApp.jsx

--- src/PinturaApp.jsx
+++ src/PinturaApp.jsx
@@ -8,20 +8,6 @@ export default function PinturaApp(){
     const [abelha, SetAbelha] = useState([80,80])
     const [seila, Setseila] = useState([150,150])
     const [quadro, Setquadro] = useState([125,125])
-    
-  useEffect(()=>{
-    console.log(larguraTabela)
-    const svg =()=>{
-    window.addEventListener('resize', svg);
-
-    // Chame a função para configurar as larguras iniciais
-    svg();
-
-    // Remover o ouvinte de evento ao desmontar o componente
-    return () => {
-      window.removeEventListener('resize', svg);
-    };
-  }, [larguraTabela]);
     return(
        <>
         <Navbar id2="Pintura"/>
